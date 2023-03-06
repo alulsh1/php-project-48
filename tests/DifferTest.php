57ce7b71b1e54943ec74d4ed872403f44d3faa3e
Aleksandr Ulshin <aleksandr.ulshin@rhsolutions.local>
2023-03-06 19:30:09 +0300

    'asdasdsadsad'

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -13,6 +13,8 @@ class DifferTest extends TestCase
     public function testReverse(): void
     {
         $test1content = file_get_contents(__DIR__  . "/fixtures/TestResult1.txt");
-        $this->assertEquals($test1content, gendiff('file1.json', 'file2.json'));
+        $t1path1 = "/file1.json";
+        $t1path2 = "/file2.json";
+        $this->assertEquals($test1content, gendiff($t1path1, $t1path2));
     }
 }
